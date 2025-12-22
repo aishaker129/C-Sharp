f04ab24193693887fc613d66f9c4fb7318a8b807
aishaker129 <aishaker129@gmail.com>
2025-12-22 23:23:03 +0600

    start learing in c sharp programming

---
+++ Problem/HelloApp/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter your input: ");
+int n = int.Parse(Console.ReadLine());
+
+while (n > 0)
+{
+    int a = int.Parse(Console.ReadLine());
+    if(a % 2 == 0)
+    {
+        Console.WriteLine("Even");
+    }
+    else
+    {
+        Console.WriteLine("Odd");
+    }
+    n--;
+}
