aee854afe274ef1ad8f61ed535f7505122fb405d
aishaker129 <aishaker129@gmail.com>
2025-12-24 00:54:19 +0600

    practice c# code

---
+++ Problem/LCMProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿int t = int.Parse(Console.ReadLine());
+while (t-->0)
+{
+    string[] parts = Console.ReadLine().Split();
+    int l = int.Parse(parts[0]);
+    int r = int.Parse(parts[1]);
+    if(l*2 > r)
+        Console.WriteLine(-1 + " " + -1);
+    else
+        Console.WriteLine(l + " " + l*2);
+}
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int t = int.Parse(Console.ReadLine());
+
+//         while (t-- > 0)
+//         {
+//             string[] parts = Console.ReadLine().Split();
+//             int l = int.Parse(parts[0]);
+//             int r = int.Parse(parts[1]);
+
+//             if (l * 2 > r)
+//                 Console.WriteLine("-1 -1");
+//             else
+//                 Console.WriteLine(l + " " + (l * 2));
+//         }
+//     }
+// }
+
---
+++ Problem/Square/Program.cs
@@ -0,0 +1,17 @@
+﻿int t=int.Parse(Console.ReadLine());
+while(t-->0)
+{
+    string[] s=Console.ReadLine().Split();
+    int a=int.Parse(s[0]);
+    int b=int.Parse(s[1]);
+    int c =int.Parse(s[2]);
+    int d=int.Parse(s[3]);
+    if(a==b && a==c && a==d)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
