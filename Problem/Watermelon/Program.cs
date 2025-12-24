3f2019ca7bff1b08f90cd1e07a82a636d4b736e8
aishaker129 <aishaker129@gmail.com>
2025-12-25 00:13:25 +0600

    watermelon cf problem with c# language

---
+++ Problem/Watermelon/Program.cs
@@ -0,0 +1,16 @@
+﻿while(true){
+	string line = Console.ReadLine();
+	if(string.IsNullOrEmpty(line))
+		break;
+	
+	int n = int.Parse(line);
+	if(n==2){
+		Console.WriteLine("NO");
+	}
+	else if(n%2==0){
+		Console.WriteLine("YES");
+	}
+	else{
+		Console.WriteLine("NO");
+	}
+}
